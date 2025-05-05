a0c7b0fc19387421f32815c3b8d30f36a7a7f6b6
bizhanchik <bizhanash@gmail.com>
2025-05-05 23:07:16 +0500

    Add files via upload

---
+++ Bailanysta/Bailanysta/BailanystaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BailanystaApp.swift
+//  Bailanysta
+//
+//  Created by Bizhan Ashykhatov on 05.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct BailanystaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bailanysta/BailanystaTests/BailanystaTests.swift
@@ -0,0 +1,17 @@
+//
+//  BailanystaTests.swift
+//  BailanystaTests
+//
+//  Created by Bizhan Ashykhatov on 05.05.2025.
+//
+
+import Testing
+@testable import Bailanysta
+
+struct BailanystaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
